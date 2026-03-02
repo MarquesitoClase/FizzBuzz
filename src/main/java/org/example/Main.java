f0004c0ccfc10b128fb5c0fe1613706b11f34d0e
MarquesitoClase <juan.luis.marquezcan@gmail.com>
2026-03-02 21:01:06 +0100

    fix: erased unused ints arraylist.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,14 +2,12 @@ package org.example;
 
 import org.example.FizzBuzz.FizzBuzz;
 
-import java.util.ArrayList;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     static void main() {
         FizzBuzz FB = new FizzBuzz();
-        ArrayList<String> lista=new ArrayList<>();
         for(int index = 0; index<30;index++){
             System.out.println(FB.convert(index));
         }
