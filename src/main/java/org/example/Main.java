8d01e31c32daaba85518e62762eb60192fc66147
MarquesitoClase <juan.luis.marquezcan@gmail.com>
2026-03-02 21:02:59 +0100

    fix: erased commentaries of java that I didn't see before

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,9 +2,6 @@ package org.example;
 
 import org.example.FizzBuzz.FizzBuzz;
 
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     static void main() {
         FizzBuzz FB = new FizzBuzz();
